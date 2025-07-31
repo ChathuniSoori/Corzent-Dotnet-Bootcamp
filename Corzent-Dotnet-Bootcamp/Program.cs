a1f18ff8f82255a250f3dbb5403d7e0bec4742a7
ChathuniSoori <chathuni.20230146@iit.ac.lk>
2025-07-31 22:01:05 +0530

    understand the startup configuration

--- Corzent-Dotnet-Bootcamp/Program.cs
+++ Corzent-Dotnet-Bootcamp/Program.cs
@@ -4,27 +4,37 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add controller services
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add API explorer services (used by Swagger)
 builder.Services.AddEndpointsApiExplorer();
+
+// Add Swagger generator
 builder.Services.AddSwaggerGen();
+
+// Register our ToDoService with dependency injection
+// This tells the system to use ToDoService whenever IToDoService is requested
 builder.Services.AddScoped<IToDoService, ToDoService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-//Enable the middleware for serving the generated JSON document and the Swagger UI
 if (app.Environment.IsDevelopment())
 {
-    
+    // Enable Swagger middleware in development
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Redirect HTTP to HTTPS
 app.UseHttpsRedirection();
 
+// Enable authorization (though we're not using it yet)
 app.UseAuthorization();
 
+// Map controller routes
 app.MapControllers();
 
+// Run the application
 app.Run();
